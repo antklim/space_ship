5bebc35c70b82bc04c62b5231816b4cf9fbf0d05
antklim <antklim@gmail.com>
2016-09-11 16:20:14 +1000

    ship module imported to main app

--- main.elm
+++ main.elm
@@ -1,2 +1,136 @@
 import Html exposing (..)
-import Html.App as SpaceShipApp
+import Html.App as ShipApp
+import Time exposing (..)
+import Keyboard
+import String exposing (..)
+import Debug exposing (..)
+
+
+import Ship
+
+main =
+  ShipApp.program
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+
+-- MODEL
+
+type alias Model =
+  { ship : Ship.Model
+  , direction : String
+  , moving : Bool
+  , shooting : Bool
+  }
+
+init : (Model, Cmd Msg)
+init =
+  ( { ship = Ship.init
+    , direction = "none"
+    , moving = False
+    , shooting = False
+    }
+  , Cmd.none
+  )
+
+
+-- UPDATE
+
+type Msg
+  = KeyDowns Keyboard.KeyCode
+  | KeyUps Keyboard.KeyCode
+  | ViewShip Ship.Msg
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    KeyDowns code ->
+      ( keyDowns model code
+      , Cmd.none
+      )
+
+    KeyUps code ->
+      ( keyUps model code
+      , Cmd.none
+      )
+
+    ViewShip msg ->
+      ( { model | ship = Ship.update msg model.ship }
+      , Cmd.none
+      )
+
+keyDowns : Model -> Keyboard.KeyCode -> Model
+keyDowns model code =
+  let
+    direction =
+      if code == 37 then
+        "left"
+
+      else if code == 39 then
+        "right"
+
+      else
+        model.direction
+
+    shooting =
+      if code == 32 then True else model.shooting
+
+    moving =
+      if code == 37 || code == 39 then True else model.moving
+
+  in
+    { model
+      | direction = direction
+      , moving = moving
+      , shooting = shooting
+    }
+
+keyUps : Model -> Keyboard.KeyCode -> Model
+keyUps model code =
+  let
+    direction =
+      if code == 37 || code == 39 then
+        "none"
+
+      else
+        model.direction
+
+    shooting =
+      if code == 32 then False else model.shooting
+
+    moving =
+      if code == 37 || code == 39 then False else model.moving
+
+  in
+    { model
+      | direction = direction
+      , moving = moving
+      , shooting = shooting
+    }
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.batch
+    [ Keyboard.downs KeyDowns
+    , Keyboard.ups KeyUps
+    ]
+
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+  div []
+    [ text (append "Mooving: " (toString model.moving))
+    , br [] []
+    , text (append "Direction: " (toString model.direction))
+    , br [] []
+    , text (append "Shooting: " (toString model.shooting))
+    , br [] []
+    , ShipApp.map ViewShip (Ship.view model.ship)
+    ]
--- ship.elm
+++ ship.elm
@@ -1,56 +1,56 @@
 module Ship exposing (Model, Msg, init, update, view)
 
+import Html exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
 -- MODEL
 
-type alias Ship =
+type alias Model =
   { position : Float
-  , velocity : Float -- 0, 1 - moving right, -1 - moving left
+  , velocity : Int -- 0, 1 - moving right, -1 - moving left
   , shooting : Bool
   }
 
-type alias Keys =
-  { x : Int -- movements -1, 0, 1
-  , y : Int -- shooting 0, 1
-  }
-
-type alias Model =
-  { ship : Ship
-  }
-
-initShip : Ship
-initShip =
+init : Model
+init =
   { position = 0
   , velocity = 0
   , shooting = False
   }
 
-init : Model
-init =
-  { ship = initShip
-  }
-
 
 -- UPDATE
+
+type Msg
+  = Move Int
+  | Shoot Bool
+  | Tick Float
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Move newVelocity ->
+      { model | velocity = newVelocity }
+
+    Shoot newShooting ->
+      { model | shooting = newShooting }
+
+    Tick dt ->
+      { model | position = model.position + dt * toFloat model.velocity }
+
+
 -- VIEW
 
-
-applyPhysics : Float -> Ship -> Ship
-applyPhysics dt ship =
-  { ship | position = ship.position + ship.velocity * dt }
-
-updateVelocity : Float -> Ship -> Ship
-updateVelocity newVelocity ship =
-  { ship | velocity = newVelocity }
-
-updateShooting : Bool -> Ship -> Ship
-updateShooting newShooting ship =
-  { ship | shooting = newShooting }
-
-updateShip : Float -> Keys -> Ship -> Ship
-updateShip dt keys ship =
-  let
-    newVel = toFloat keys.x
-    isShooting = keys.y > 0
-
-  in
-    updateVelocity newVel (updateShooting isShooting (applyPhysics dt))
+view : Model -> Html Msg
+view model =
+  Svg.svg [ viewBox "0 0 60 60", width "300", height "300" ]
+    [ rect [ x "0"
+           , y "0"
+           , width "60"
+           , height "60"
+           , stroke "black"
+           , strokeOpacity "0.5"
+           , fillOpacity "0" ] []
+    , rect [ x "28", y "58.5", width "3", height "1", fill "#0B79CE" ] []
+    ]
